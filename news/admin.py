88dcf64cca08002e13d4e72c444b412abc0e61ed
edzen12 <oichiev.edzen@gmail.com>
2024-12-18 19:15:00 +0600

    edit

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import News, SliderHomepage
+from .models import News, SliderHomepage,PaymentMethod
 
 
 admin.site.register(News)
 admin.site.register(SliderHomepage)
+admin.site.register(PaymentMethod)
--- news/models.py
+++ news/models.py
@@ -22,9 +22,22 @@ class News(models.Model):
         verbose_name="Описание"
     )
     image = models.ImageField(upload_to='service', null=True, blank=True)
+    
     def __str__(self):
         return self.title
 
     class Meta:
         verbose_name_plural = 'Услуги'
         verbose_name = 'услуга'
+
+
+class PaymentMethod(models.Model):
+    title = models.CharField(max_length=50)
+    img = models.ImageField(upload_to='payMethod')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Способы оплаты'
+        verbose_name = 'способ оплаты'
